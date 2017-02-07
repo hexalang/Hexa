830d2f40cfe8e168205fbe06da35f0334c794e8e
Oleg <alegorium@gmail.com>
2017-02-07 13:38:19 +0300

    Some boilerplate to run converter

---
+++ bootstrap/Converter.hx
@@ -0,0 +1,15 @@
+import haxe.macro.Type;
+import haxe.macro.Expr;
+import sys.io.File;
+
+using Lambda;
+using StringTools;
+using haxe.macro.TypedExprTools;
+
+class Converter {
+
+#if macro
+	public static function use() {
+	}
+#end
+}
