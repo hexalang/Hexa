c61f52421558ac7c9769695565696d13193cfdb1
Oleg <alegorium@gmail.com>
2017-02-12 23:45:05 +0300

    Mocks for testing

---
+++ bootstrap/Main.hx
@@ -0,0 +1,34 @@
+// The Hexa Compiler
+// Copyright (C) 2017  Oleg Petrenko
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import NodeJs;
+import Lexer;
+import Parser;
+import Typer;
+
+import TestLexer;
+import TestParser;
+import TestTyper;
+
+using StringTools;
+
+class Main {
+	function new() {
+	}
+
+	static function main() new Main();
+}
