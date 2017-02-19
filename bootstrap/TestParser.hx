c528d50aff347fdade401ba7e8871dbb466f792f
Oleg <alegorium@gmail.com>
2017-02-19 21:09:45 +0300

    Parser basic tests

--- bootstrap/TestParser.hx
+++ bootstrap/TestParser.hx
@@ -15,9 +15,56 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+import Data;
+import Token;
+import Lexer;
+import Parser;
+import NodeJs;
+
+using TestParser;
+using StringTools;
+
 class TestParser {
 	public static function test() {
 		trace("TestParser begin");
-		trace("TestParser end");
+		var input = '1 2 3 trace("Hello!", "World!") + 5 * 6 / 3';
+		var test = 'TBlock([
+				   TInt(1),
+				   TInt(2),
+				   TInt(3),
+				   TBinop(+,
+				   TCall(TIdent(trace),[TString(Hello!),TString(World!)]),
+				   TBinop(*,TInt(5),TBinop(/,TInt(6),TInt(3))))
+				   ])'.replace('\n', '').replace('\r', '').replace('\t', '').replace(' ', '');
+		var lexe = Lexer.tokenize(Buffer.from(input));
+		var parser = new Parser(lexe);
+		if (test != stringify(parser.result)) throw "TestParser test fail";
+		trace("TestParser done");
+	}
+
+	public static function stringify(node: Node): String {
+		return switch (node) {
+
+		// Have no sub-nodes
+		case TString(s): 'TString($s)';
+		case TIdent(s): 'TIdent($s)';
+		case TBool(b): 'TBool($b)';
+		case TThis: 'TThis';
+		case TSuper: 'TSuper';
+		case TInt(s): 'TInt($s)';
+		case TFloat(s): 'TFloat($s)';
+		case TNull: 'TNull';
+		case TBreak: 'TBreak';
+		case TContinue: 'TContinue';
+
+		// Have sub-nodes
+		case TBinop(op, a, b): 'TBinop(' + op.stringify() + ',' + a.stringify() + ',' + b.stringify() + ')';
+		case TBlock(el): 'TBlock([' + [for (e in el) e.stringify()].join(',') + '])';
+		case TCall(e, el): 'TCall(' + e.stringify() + ',[' + [for (e in el) e.stringify()].join(',') + '])';
+		case TParenthesis(e): 'TParenthesis(' + e.stringify() + ')';
+		case TReturn(e): 'TReturn(' + e.stringify() + ')';
+		case TThrow(e): 'TThrow(' + e.stringify() + ')';
+		case e: '<!--' + e + '-->';
+		}
 	}
 }
