cd78dbc02f5e5e50b34a11788a6126c8beca7952
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2018-01-04 02:59:02 +0300

    `hexa init` command

--- bootstrap/Main.hx
+++ bootstrap/Main.hx
@@ -20,14 +20,77 @@ import NodeJs;
 import Lexer;
 import Parser;
 import Typer;
+import Data;
 
 using StringTools;
 
-@:keep
+@:enum
+abstract Generator(String) {
+  var JavaScript = "JavaScript";
+  var C = "C";
+  var Haxe = "Haxe";
+}
+
+typedef Target = {
+	generator: Generator,
+	instance: Dynamic
+}
+
+typedef Package = {
+	name: String,
+	version: String,
+	description: String,
+	main: String,
+	author: String,
+	license: String,
+	target: Target,
+	files: Array<String>
+};
+
 /**
  * @author PeyTy
  **/
 class Main {
+	function init() {
+		var currentDirTmp = Process.argv[1].split(Path.sep);
+		var packageFileName = currentDirTmp.splice(0, -1).join(Path.sep) + Path.sep + "hexa.json";
+		var currentDir = currentDirTmp[currentDirTmp.length - 2];
+		var name = Readline.question('Enter package name ($currentDir): ');
+		if(name == "") name = currentDir;
+
+		var version = Readline.question("Enter package version (1.0.0): ");
+		if(version == "") version = "1.0.0";
+
+		var description = Readline.question("Enter package description: ");
+
+		var main = Readline.question("Enter main file (main.hexa): ");
+		if(main == "") main = "main.hexa";
+
+		var target: Target = { generator: Generator.JavaScript, instance: { generatePackageJson: false } };
+		var generatorstr = Readline.question("Enter generator type (JavaScript):\n1. JavaScript\n2. C\n3. Haxe");
+		if(generatorstr.toUpperCase() == "C" || generatorstr == "2") target.generator = Generator.C;
+		else if(generatorstr.toUpperCase() == "HAXE" || generatorstr == "3") target.generator = Generator.Haxe;
+
+		while(!(main.endsWith(".hexa"))) {
+			Process.stdout.write("Wrong file name. Must ends with '.hexa'.\n");
+			main = Readline.question("Enter main file (main.hexa): ");
+		}
+
+		var author = Readline.question("Enter package author name: ");
+		var license = Readline.question("Enter license: ");
+
+		var pack: Package = {
+			name: name, version: version, description: description,
+			main: main, author: author, license: license, target: target, files: []
+		};
+		var packageStr = haxe.Json.stringify(pack, null, "\t");
+		var answ = Readline.question('About to write to: $packageFileName:\n$packageStr\nIs this ok? (yes)');
+		if(answ != "") Process.exit(1);
+
+		Fs.writeFileSync("hexa.json", packageStr);
+		Process.stdout.write("Hexa package initialized!\n");
+	}
+
 	function new() {
 		// Debug information
 		untyped __js__("require('source-map-support').install()");
@@ -39,9 +102,14 @@ class Main {
 		TestParser.test();
 		#end
 
+		// Usage examples
 		if(Process.argv[2] == null) {
-			trace("Usage: hexa project.json");
+			trace("Usage: `node hexa.js project.json` or `node hexa.js init`");
 			Process.exit(1);
+		}
+		else if(Process.argv[2] == "init") {
+			init();
+			Process.exit(0);
 		}
 
 		// Initialize compiler
