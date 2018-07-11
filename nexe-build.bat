0d6408136c824f9718f3447de5e48fe8a8db4149
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2018-07-11 03:23:37 +0300

    Update build instructions

--- nexe-build.bat
+++ nexe-build.bat
@@ -1,5 +1,6 @@
 nexe hexa.js --target win32-x86-9.8.0 --ico icon.ico --name hexa -o hexa
 set ResourceHacker="ResourceHacker.exe"
+@echo off
 if exist %ResourceHacker% (
 	echo Replacing icon...
 	%ResourceHacker% -addoverwrite "hexa.exe", "hexa.exe", "icon.ico", ICONGROUP, MAINICON, 0
