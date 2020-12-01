f38be8ba0a61066095aa1c0ec3a8e8edc5d1b00a
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-12-01 18:00:46 +0000

    [Misc] Edit build script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo on
 cd %~dp0
-echo [[Build started]]
-node bootstrap.js hexa.json
-node hexa-node.js hexa.json
-node hexa-node.js hexa.json
-echo [[Build complete]]
+@echo [[Build started]]
+node bootstrap.js --define entry=false hexa.json
+node hexa-node.js --define entry=false --define debug=true --define times=true hexa.json
+node hexa-node.js --define entry=false --define debug=false --define times=true hexa.json
+@echo [[Build complete]]
