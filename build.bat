e01f08439b6e220ef17a88c40a6ec98c6a5479a9
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-12-04 13:45:04 +0000

    [Misc] Update bootstrap.js

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo on
 cd %~dp0
 @echo [[Build started]]
-node bootstrap.js --define entry=false hexa.json
-node hexa-node.js --define entry=false --define debug=true --define times=true hexa.json
-node hexa-node.js --define entry=false --define debug=false --define times=true hexa.json
+node bootstrap.js --define debug=false hexa.json
+node hexa-node.js --define debug=true --define times=false hexa.json
+node hexa-node.js --define debug=false --define times=false hexa.json
 @echo [[Build complete]]
