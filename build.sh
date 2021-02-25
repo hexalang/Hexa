a644ff7d75cb3505a2fa369769e748e35c1d4606
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2021-02-25 19:50:46 +0000

    [Typer] Wildcard imports, more module warning

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # Call me as `bash build.sh`
-echo [[Build started]]
+echo Welcome to the Hexa! Support us at https://www.patreon.com/PeyTy and https://hexalang.github.io/donate/
 cd $(dirname "$0")
 node bootstrap.js hexa.json
 node hexa-node.js hexa.json
