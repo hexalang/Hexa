7c7df65be36b3ccd3b4eb3baf591fa2871156e7d
acidicMercury8 <acidicmercury8@outlook.com>
2022-05-29 15:22:44 +0300

    [Misc] Update build script for libgen
    [skip ci]

--- tools/libgen/build.bat
+++ tools/libgen/build.bat
@@ -1,2 +1,3 @@
+@echo off
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cl /INCREMENTAL:NO /Os main.cpp
+cl main.cpp /Felibgen.exe /Os /INCREMENTAL:NO /nologo
